52c289d7995297c249b9092572dfbd4fadfded64
Venkatesh Madireddy <venkateshmadireddy123@gmail.com>
2025-05-02 23:07:40 +0530

    code upated

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ top_students = df[df["marks"] > 80]
 
 print("Students with marks > 80:")
 print(top_students["name"].to_list())
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Accept name and age.")
+parser.add_argument('--name', type=str, required=True, help="Your name")
+parser.add_argument('--age', type=int, required=True, help="Your age")
+
+args = parser.parse_args()
+
+print(f"Welcome {args.name}, your age is {args.age}!")
